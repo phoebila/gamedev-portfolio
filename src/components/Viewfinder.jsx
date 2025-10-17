33ebc1d7fe3817d982b373854de5b06b14173600
Phoebe <128556139+phoebila@users.noreply.github.com>
2025-10-17 12:26:23 -0700

    Base push

---
+++ src/components/Viewfinder.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Viewfinder = () => {
+  return (
+    <>
+      
+      {/* <div className="timer">余永康</div> */}
+      <div className="location">Sunnyvale, California</div>
+      <div className="quality">&copy; Phoebe Royer 2025</div>
+    </>
+  );
+};
+
+export default Viewfinder;
